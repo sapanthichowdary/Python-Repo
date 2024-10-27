5b6e857bf87cda78631b08160a985d17c8ebdd36
sapanthichowdary <147791772+sapanthichowdary@users.noreply.github.com>
2024-10-27 16:11:20 +0530

    Uploading code: Character is Uppercase or Lowercase.py

---
+++ Character is Uppercase or Lowercase.py
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Alphabet
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        if (ch >= 'A' && ch <='Z')
+        {
+            System.out.println("uppercase alphabet");
+        }
+        else if(ch >= 'a' && ch <= 'z')
+        {
+            System.out.println("lowercase alphabet");
+        }
+        else{
+            System.out.println("not an alphabet");
+        }
+    }
+}
