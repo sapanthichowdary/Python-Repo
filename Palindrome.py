61d06a13f678d25ebdd32c96e1e87078ac3d975e
sapanthichowdary <147791772+sapanthichowdary@users.noreply.github.com>
2024-10-30 16:01:46 +0530

    Uploading code: Palindrome.py

---
+++ Palindrome.py
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Palindrome
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int t=n;
+        int rev = 0;
+        while(n!=0)
+        {
+            int r = n%10;
+            rev=rev*10+r;
+            n=n/10;
+        }
+        if(rev==t)
+        {
+            System.out.println("Palindrome");
+        }
+        else
+        {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
